3c2e60fde7cf184d4c493d0e0fddc1aaa2a1f7de
kshiti.sinha <kshiti.sinha@myndsol.com>
2022-07-04 18:54:31 +0530

    initial commit

--- AVP_Report.py
+++ AVP_Report.py
@@ -8,34 +8,35 @@ avp_report = pd.DataFrame(
     columns=['Code', 'Name', 'Category', 'Line/Section', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
              '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
              '29', '30', '31', 'Grand Total'])
-print(old_attendance_report)
 col_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
             '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 new_header = old_attendance_report.iloc[0]
 old_attendance_report = old_attendance_report[1:]
 old_attendance_report.columns = new_header
-avp_report['date_value'] = pd.to_datetime(old_attendance_report['Date']).dt.day.astype(str)
+old_attendance_report['date_value'] = pd.to_datetime(old_attendance_report['Date']).dt.day.astype(str)
+emp_value = old_attendance_report['Employee Code'].unique()
+old_attendance_report
 
-# for i,row in avp_report.iterrows():
-#     if row['date_value'] ==
+avp_report['Code'] = old_attendance_report['Employee Code'].drop_duplicates()
+avp_report['Name'] = old_attendance_report['Employee Name'].drop_duplicates()
+avp_report['Category'] = old_attendance_report['Employee Type'].drop_duplicates()
+avp_report['Line/Section'] = old_attendance_report['Function'].drop_duplicates()
+arrival = pd.to_datetime(old_attendance_report['Actual In Time'], format='%H:%M:%S')
+departure = pd.to_datetime(old_attendance_report['Actual Out Time'], format='%H:%M:%S')
+old_attendance_report['hours'] =abs((arrival - departure).astype('timedelta64[h]'))
 
-# for i,row in old_attendance_report.iterrows():
-#     print(row['Date'])
-# avp_report.index = [x for x in range(1, len(avp_report.values)+1)]
-avp_report['Code'] = old_attendance_report['Employee Code']
-avp_report['Name'] = old_attendance_report['Employee Name']
-avp_report['Category'] = old_attendance_report['Employee Type']
-avp_report['Line/Section'] = old_attendance_report['Function']
-# len(avp_report)
-# avp_report = avp_report.drop_duplicates(subset=['Code','Name','Category','Function'])
-# len(avp_report)
+
+for i,row in old_attendance_report.iterrows():
+    for j,row2 in avp_report.iterrows():
+        if row['date_value'] == 1:
+            row2['1'] == old_attendance_report['hours']
+            print(row2['1'])
+
+print(avp_report)
 
 avp_report.to_excel('AVP_Report.xlsx')
 
 # avp_report['Grand Total'] = old_attendance_report[col_list].sum(axis=1)
 
-# print(avp_report)
-
-# print(avp_report)
 
 
--- Daily_Attendance_Report.py
+++ Daily_Attendance_Report.py
@@ -1,10 +1,11 @@
+
 import pandas as pd
 import numpy as np
 import datetime
 from datetime import datetime, date
 
-daily_attendance_old = pd.read_excel(r"C:\Users\kshiti.sinha\Desktop\projects\Shri Ram Pistons Reports\input\Daily_Attendance_Report.xlsx",
-    sheet_name="Sheet1")
+daily_attendance_old = pd.read_excel(r"C:\Users\hrithik.chauhan\Downloads\Daily_Attendance_Report.xlsx",
+                                     sheet_name="Sheet1")
 
 daily_attendance_new = pd.DataFrame(
     columns=['SR NO', 'CODE', 'NAME', 'DATE', 'SHIFT', 'ARRIVAL', 'DEPARTURE', 'WORK', 'Total', 'STATUS', 'PIR'])
@@ -12,20 +13,22 @@ daily_attendance_new = pd.DataFrame(
 # daily_attendance_new['SR NO'] = daily_attendance_old['']
 daily_attendance_new['CODE'] = daily_attendance_old['Employee Code']
 daily_attendance_new['NAME'] = daily_attendance_old['Employee Name']
+daily_attendance_new['Shift_Name'] = daily_attendance_old['Shift Name']
+
 daily_attendance_new['DATE'] = daily_attendance_old['Date'].dt.date.astype(str)
 
-for i,row in daily_attendance_old.iterrows():
-    if row['Shift Name'] == "06:00 AM to 02:30 PM":
-        daily_attendance_new['SHIFT'] = "A"
-    elif row['Shift Name'] == "14:00 PM to 22:30 PM":
-        daily_attendance_new['SHIFT'] = "B"
-    elif row['Shift Name'] == "22:00 PM to 06:30 AM":
-        daily_attendance_new['SHIFT'] = "C"
-    elif row['Shift Name'] == "09:00 AM to 17:30 PM":
-        daily_attendance_new['SHIFT'] = "G"
-    else:
-        daily_attendance_new['SHIFT'] = "no shift"
+for i, row in daily_attendance_new.iterrows():
+    if row['Shift_Name'] == "06:00 AM to 02:30 PM":
 
+        row['SHIFT'] = "A"
+    elif row['Shift_Name'] == "14:00 PM to 22:30 PM":
+        row['SHIFT'] = "B"
+    elif row['Shift_Name'] == "22:00 PM to 06:30 AM":
+        row['SHIFT'] = "C"
+    elif row['Shift_Name'] == "09:00 AM to 17:30 PM":
+        row['SHIFT'] = "G"
+    else:
+        row['SHIFT'] = "no shift"
 
 
 daily_attendance_new['ARRIVAL'] = daily_attendance_old['In Time']
@@ -34,29 +37,28 @@ arrival = pd.to_datetime(daily_attendance_old['In Time'], format='%H:%M:%S')
 
 daily_attendance_new['DEPARTURE'] = daily_attendance_old['Out Time']
 departure = pd.to_datetime(daily_attendance_old['Out Time'], format='%H:%M:%S')
-daily_attendance_new['WORK'] = arrival - departure
-daily_attendance_new['WORK'].dt.days.astype(float)
+daily_attendance_new['WORK'] = abs((arrival - departure).astype('timedelta64[h]'))
 
-for i,row in daily_attendance_old.iterrows():
-        if row['Actual Status'] == "Present":
-            row['STATUS'] = "P"
-        elif row['Actual Status'] == "Absent":
-            row['STATUS'] = "A"
-        else:
-            row['STATUS'] = "CL for now"
 
-for i,row in daily_attendance_new.iterrows():
+
+daily_attendance_new['Total'] = "0.0"
+daily_attendance_new['STATUS'] = daily_attendance_old['Actual Status']
+
+count = 0;
+for i, row in daily_attendance_new.iterrows():
+
     if row['WORK'] >= float(8.0):
-        daily_attendance_new['PIR'] = "1.00"
-    elif row['WORK'] <= float(4.0):
-        daily_attendance_new['PIR'] = "0.50"
+        temp = i + 2
+        count = count + 1
+        daily_attendance_new.loc[i, 'PIR'] = "1.00"
+    elif row['WORK'] == float(4.0):
+        temp = i + 2
+        count = count + 1
+        daily_attendance_new.loc[i, 'PIR'] = "0.50"
     else:
-        daily_attendance_new['PIR'] = "0.00"
-
+        daily_attendance_new.loc[i, 'PIR'] = "0.00"
 
 daily_attendance_new.replace(np.nan,"",regex=True)
 
 
-# daily_attendance_new['WORK'] =
-
-daily_attendance_new.to_excel('daily_attendance_report.xlsx',columns=['CODE', 'NAME', 'DATE', 'SHIFT', 'ARRIVAL', 'DEPARTURE', 'WORK', 'Total', 'STATUS', 'PIR'])
+daily_attendance_new.to_excel(r'C:\Users\hrithik.chauhan\Desktop\format.xlsx', columns=['SR NO','CODE'	,'NAME'	,'DATE'	,'SHIFT','ARRIVAL','DEPARTURE','WORK'  ,'Total' ,'STATUS','PIR'])
